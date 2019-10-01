6109232391c6667145dcecd7fac8b51dd08ba446
quinton22 <quintonhoffman22@gmail.com>
2019-10-01 11:59:05 -0500

    not working gravity

---
+++ Solar-System-Simulation/Assets/Scripts/GravitationalBody.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravitationalBody : MonoBehaviour
+{
+    public float mass;
+    public Vector3 initialVelocity;
+    public Quaternion rotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = initialVelocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

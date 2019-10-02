e96bd8e0c01400a08e51fb1bee87b52184d831b7
quinton22 <quintonhoffman22@gmail.com>
2019-10-01 21:28:46 -0500

    working solar system scaled down

---
+++ Solar-System-Simulation/Assets/Scripts/SolarSystemManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolarSystemManager : MonoBehaviour
+{
+    public float SizeScale = 1;
+    public float TimeScale = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

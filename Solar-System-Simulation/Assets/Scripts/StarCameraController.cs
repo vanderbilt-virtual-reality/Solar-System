a1ee0d72ccb684a1689e60a789f6ddf9160a4f30
Than <viet.than@vanderbilt.edu>
2019-12-04 16:08:16 -0600

    fixed starry camera, fixed sun transparency, fixed spaceship size

---
+++ Solar-System-Simulation/Assets/Scripts/StarCameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class StarCameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = -UnityEngine.XR.InputTracking.GetLocalPosition(UnityEngine.XR.XRNode.CenterEye);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = -UnityEngine.XR.InputTracking.GetLocalPosition(UnityEngine.XR.XRNode.CenterEye);
+    }
+}

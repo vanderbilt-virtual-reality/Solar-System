245867a968f804b9638c3784ad7ae9cfbd919a4a
Than <viet.than@vanderbilt.edu>
2019-12-08 05:46:37 -0600

    random planet starting positions

--- Solar-System-Simulation/Assets/Scripts/SolarSystemManager.cs
+++ Solar-System-Simulation/Assets/Scripts/SolarSystemManager.cs
@@ -20,18 +20,32 @@ public class SolarSystemManager : MonoBehaviour
     private CharacterMovement mCharacterMovement;
     private PlanetScale[] PlanetScales;
     private bool CollidersOn = true;
-
+    [SerializeField] private float maxTimeAdvance = 1000000000;
+    private float advanceTimer = 1;
+    private float oldTimeScale;
+    private float newTimeScale;
     // Start is called before the first frame update
     void Start()
     {
         orbiters = FindObjectsOfType<Orbiter>();
         mCharacterMovement = Character.GetComponent<CharacterMovement>();
         PlanetScales = GameObject.FindGameObjectsWithTag("Planet").Select(go => new PlanetScale { go=go, scale=go.transform.localScale}).ToArray();
+        oldTimeScale = TimeScale;
+        newTimeScale = Random.value * maxTimeAdvance + maxTimeAdvance;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (advanceTimer > 0)
+        {
+            advanceTimer -= Time.deltaTime;
+            TimeScale = newTimeScale;
+        } else if (advanceTimer <= 0 && advanceTimer != -100) {
+            TimeScale = oldTimeScale;
+            advanceTimer = -100;
+        }
+
         SolarSystem.transform.localScale = Vector3.one * SizeScale;
 
         if (PlanetOnlyScale > 1 && CollidersOn) {

1bc8b7b86a265fdcd3577dda09918a46a0980cce
Quinton Hoffman <quintonhoffman22@gmail.com>
2019-12-09 01:54:42 -0600

    buggy trigger enter

--- Solar-System-Simulation/Assets/Scripts/MockOrbiter.cs
+++ Solar-System-Simulation/Assets/Scripts/MockOrbiter.cs
@@ -4,31 +4,57 @@ using UnityEngine;
 
 public class MockOrbiter : MonoBehaviour
 {
+
+    private MockSolarSystemManager manager;
     public GameObject ReferenceObject;
+    private float collisionDelay = 0.5f;
+    private bool isColliding = false;
     void Start()
     {
+        manager = transform.parent.GetComponent<MockSolarSystemManager>();
         Transform SolarSystemTransform = GameObject.Find("SolarSystem").transform;
         string searchString = gameObject.name.Substring(4);
 
         if (searchString == "Sun")
         {
             ReferenceObject = SolarSystemTransform.Find(searchString).gameObject;
-        } else
+        }
+        else
         {
             ReferenceObject = SolarSystemTransform.Find(searchString + "System").gameObject;
         }
     }
 
+    private void Update()
+    {
+        if (collisionDelay <= 0)
+        {
+            isColliding = false;
+            collisionDelay = 0.5f;
+        }
+        if (isColliding)
+        {
+            collisionDelay -= Time.deltaTime;
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
+        if (isColliding) return;
+        isColliding = true;
+        Debug.Log($"enter {gameObject.name}");
         // other.gameObject.name == RightHandAnchor || LeftHandAnchor
         if (other.gameObject.name == "RightHandPointerCollider" || other.gameObject.name == "LeftHandPointerCollider")
         {
             Debug.Log($"here2: {other.gameObject.name}, {gameObject.name}");
-            MockSolarSystemManager manager = transform.parent.GetComponent<MockSolarSystemManager>();
             manager.SelectedPlanets[gameObject.name] = !manager.SelectedPlanets[gameObject.name];
             manager.updateSelectedPlanets();
         }
-     
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log($"exit {gameObject.name}");
     }
 }
